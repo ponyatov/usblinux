022d0f8e0028d55d8361502e1f0cd030d2bb7fc5
Dmitry Ponyatov <dponyatov@gmail.com>
2014-06-02 13:28:19 +0400

    + download netinst.iso

--- Debian/get.bat
+++ Debian/get.bat
@@ -1,3 +1,4 @@
 wget -N http://http.us.debian.org/debian/dists/stable/main/installer-i386/current/images/hd-media/vmlinuz
 wget -N http://http.us.debian.org/debian/dists/stable/main/installer-i386/current/images/hd-media/initrd.gz
+wget -N http://cdimage.debian.org/debian-cd/current/i386/iso-cd/debian-7.5.0-i386-netinst.iso
 pause
