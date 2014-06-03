17f983b7e73780dbaeff6e0caac8517bec0b8aa7
Dmitry Ponyatov <dponyatov@gmail.com>
2014-06-03 11:28:38 +0400

    * Debian/(i386|amd64) + /misc

---
+++ Debian/amd64/get64.bat
@@ -0,0 +1,4 @@
+wget -N http://http.us.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/vmlinuz
+wget -N http://http.us.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/initrd.gz
+wget -N http://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-7.5.0-amd64-netinst.iso
+pause
